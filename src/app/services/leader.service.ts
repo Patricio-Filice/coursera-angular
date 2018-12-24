dd1c4ab61c4d1332d1cb82487b86e481cc891f7b
Patricio Leonel Filice <patricio.filice@gmail.com>
2018-12-24 01:53:05 -0300

    Assignment 2

---
+++ src/app/services/leader.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { LEADERS } from '../shared/leaders';
+import { Leader } from '../shared/leader';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LeaderService {
+
+  constructor() { }
+
+  getLeaders(): Leader[] {
+    return LEADERS;
+  }
+
+  getLeader(id: string): Leader {
+    return LEADERS.find(leader => leader.id === id);
+  }
+
+  getFeaturedLeader(): Leader {
+    return LEADERS.find(leader => leader.featured);
+  }
+}
